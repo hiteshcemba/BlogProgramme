be081b9e32be3ce9e21cbee8adfdf07d735930c9
Hitesh Prajapati <hitesh.cemba@outlook.com>
2017-08-10 14:55:25 -0400

    commit1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Today.ToString("dd ") + DateTime.Today.ToString("y", CultureInfo.CreateSpecificCulture("ro-RO")));
+            Console.Read();
+        }
+    }
+}
